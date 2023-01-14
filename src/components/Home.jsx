ad00b8bcf34c977bb5c28b9fb3455aa3a571715b
eliasndegwa <eliasndegwamwai@gmail.com>
2023-01-14 22:06:01 +0300

    use click event in button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,15 @@
 const Home = () => {
+    const handleClick = () =>{
+        console.log("Hello!")
+    }
+    const handleClickMeAgain = (name) =>{
+        console.log("hello " + name)
+    }
     return (
         <div className="Home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={()=>handleClickMeAgain('me')}>Click Me Again</button>
         </div>
     );
 }
